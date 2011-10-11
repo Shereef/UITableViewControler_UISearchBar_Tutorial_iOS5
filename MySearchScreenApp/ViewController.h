1f76564cebc7db7e6336aa97fc01070e04707958
Shereef Marzouk (work) <work@Shuttle.local>
2011-10-11 12:05:56 +0200

    Initial Commit

---
+++ MySearchScreenApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MySearchScreenApp
+//
+//  Created by Shereef Marzouk on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MySearchScreenApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySearchScreenApp
+//
+//  Created by Shereef Marzouk on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
